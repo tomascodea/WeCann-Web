2e1fa135592154c66d2acd6bb14f72ed914d0df1
Tomas Vazquez <tvazquez@epagos.com.ar>
2024-07-01 13:26:57 -0300

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { Box, Flex, Link, Button, Menu, MenuButton, MenuList, MenuItem, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, useDisclosure } from '@chakra-ui/react';
+import { ChevronDownIcon, HamburgerIcon } from '@chakra-ui/icons';
+import Image from 'next/image';
+import logo from '../../public/brand/WeCann.png';
+
+export default function Header() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const linkStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    position: 'relative' as const,
+    paddingBottom: '4px',
+    height: 'full',
+    _hover: {
+      textDecoration: 'none',
+      _after: {
+        width: '100%',
+      },
+    },
+    _after: {
+      content: '""',
+      position: 'absolute' as const,
+      top: '40px',
+      left: 0,
+      width: '0',
+      height: '2.5px',
+      bg: '#00BF30',
+      transition: 'width 0.3s ease-in-out',
+    },
+  };
+
+  const linkStylesMobile = {
+    display: 'flex',
+    alignItems: 'center',
+    position: 'relative' as const,
+    paddingBottom: '4px',
+    fontWeight: 'bold',
+    _hover: {
+      textDecoration: 'none',
+      color: '#00BF30',
+      transition: 'color 0.3s ease-in-out',
+    }
+  };
+
+  return (
+    <Box as='nav' bg="white" boxShadow="md">
+      <Flex maxW="7xl" mx="auto" px={4} justify="space-between" align="center" h="16">
+        <Box>
+          <Image src={logo} alt="Logo" width={125} height={50} />
+        </Box>
+        <Flex display={{ base: 'none', md: 'flex' }} align="center" flex="1" justify="flex-end">
+          <Link href="#" ml={4} {...linkStyles}>
+            Home
+          </Link>
+          <Link href="#" ml={4} {...linkStyles}>
+            ONGs
+          </Link>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />} ml={4}>
+              Redes Sociales
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Instagram</MenuItem>
+              <MenuItem>LinkedIn</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+
+        <Box display={{ base: 'flex', md: 'none' }} onClick={onOpen}>
+          <HamburgerIcon w={6} h={6} />
+        </Box>
+      </Flex>
+
+      <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">Menu</DrawerHeader>
+          <DrawerBody>
+            <Link href="#" w="100%" mb={4} {...linkStylesMobile}>
+              Home
+            </Link>
+            <Link href="#" w="100%" mb={4} {...linkStylesMobile}>
+              ONGs
+            </Link>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+}
